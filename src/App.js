9d13f3a6e44b421dbd7ea1b974ff32d5eb5b8785
Milly <yehm@umich.edu>
2019-05-16 21:19:38 -0400

    got data, changed what is on state

--- src/App.js
+++ src/App.js
@@ -3,29 +3,36 @@ import {APP_ID, APP_URL} from './config/environment.js'
 import './App.css';
 
 class App extends React.Component {
-  constructor() {
-    super() 
+  constructor(props) {
+    super(props) 
     this.state = {
       name: '',
       temp: null,
       description: ''
     }
+    // this.getWeatherInfo = this.getWeatherInfo.bind(this)
   }
   
   componentDidMount() {
+    this.getWeatherInfo()
   }
 
-  getWeatherInfo = async () => {
-    navigator.geolocation.getCurrentPosition(async function(position) {
+  getWeatherInfo () {
+    // console.log('state',this.state)
+    navigator.geolocation.getCurrentPosition(async(position) => {
       let lat = position.coords.latitude
       let lon = position.coords.longitude
 
       try {
         const res = await fetch(`${APP_URL}?lat=${lat}&lon=${lon}&units=metric&APPID=${APP_ID}`);
         const weatherData = await res.json();
-        console.log(weatherData.name, weatherData.main.temp, weatherData.weather[0].description)
-        //conversion (7.1°C × 9/5) + 32 = 44.78°F
-        return weatherData;
+        let fahrenheight = (weatherData.main.temp)*(9/5) + 32
+        // console.log('state:',this.state)
+        this.setState({name: weatherData.name,
+                      temp: fahrenheight,
+                      description: weatherData.weather[0].description})
+        console.log('state after', this.state)
+        return weatherData
       } catch (err) {
         console.error(err)
       }
@@ -33,11 +40,11 @@ class App extends React.Component {
   }
 
   render() {
-    this.getWeatherInfo()
+    // this.getWeatherInfo()
     return (
       <div className="App">
         <header className="App-header">
-        Welcome, the weather in your area is: 
+        Welcome, the weather in your area is:
         </header>
         <h3>View song recommendations: </h3>
       </div>
