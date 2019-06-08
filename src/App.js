8cbe9b3494b79d385fa229b6101004c2df3c01ec
Milly <yehm@umich.edu>
2019-06-08 15:43:50 -0400

    changed querystringify version in package-lock.json file to resolve security vulnerability warning

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import React from 'react';
 import {APP_ID, APP_URL} from './config/environment.js'
 import axios from 'axios'
 import { Button, Icon, Grid, Image, Header, Popup } from 'semantic-ui-react'
+import styled from 'styled-components' //need this to use styled spinkit
+import { CubeGrid, WaveLoading } from 'styled-spinkit'
 import './App.css';
 
 class App extends React.Component {
@@ -11,22 +13,21 @@ class App extends React.Component {
       name: '',
       temp: null,
       description: '',
-      songs: []
+      songs: [],
+      weatherLoading: true
     }
     this.getWeatherInfo = this.getWeatherInfo.bind(this)
     this.handleClick = this.handleClick.bind(this)
   }
   
   async componentDidMount() {
-    let weatherInfo = this.getWeatherInfo()
-    console.log(weatherInfo)
+    this.getWeatherInfo()
+    // console.log(weatherInfo)
     // console.log(tracks)
     // console.log('TEST IMG URL', tracks[0].track.album.images[0].url) //this works
   }
 
   async handleClick() {
-    //I think I should set state in here instead of componentDidMount
-    //Try this, if doesn't work then need to move it back up
     if (this.state.temp <= 50) {
       const sadResult = await axios.get('/api/songs/sad')
       const sadTracks = sadResult.data.slice(0,10)
@@ -50,7 +51,8 @@ class App extends React.Component {
         let fahrenheight = Math.round((weatherData.main.temp)*(9/5) + 32)
         this.setState({name: weatherData.name,
                       temp: fahrenheight,
-                      description: weatherData.weather[0].description})
+                      description: weatherData.weather[0].description,
+                      weatherLoading: false})
         return weatherData
       } catch (err) {
         console.error(err)
@@ -61,29 +63,37 @@ class App extends React.Component {
   render() {
     return (
       <div className="App">
-        <Header as='h2' icon id='weather'>
-          <Icon name='cloudversify' />
-          {this.state.name}
-          <Header.Subheader>{this.state.temp}&deg;F</Header.Subheader>
-          <Header.Subheader>{this.state.description}</Header.Subheader>
-      </Header>
-        
-      <Grid relaxed columns={4}>
-        {this.state.songs.map(song => {
-            return (
-              <Grid.Column>
-                <Popup trigger={<Image alt='' src={song.track.album.images[0].url}/>} 
-                  content={`Track name: ${song.track.name}         
-                                Artist: ${song.track.artists[0].name}`}
-                  style={style}
-                />
-              </Grid.Column>
-            )
-          }
-          )}
-      </Grid>
-        
-        <Button animated='fade' onClick={this.handleClick}>
+        {this.state.weatherLoading ? (
+          <WaveLoading color='white' size={80}/>
+        ) : (
+          <Header as='h1' icon id='weather'>
+            <Icon name='cloudversify' />
+            {this.state.name}
+            <Header.Subheader>{this.state.temp}&deg;F</Header.Subheader>
+            <Header.Subheader>{this.state.description}</Header.Subheader>
+          </Header>
+        )}
+
+        {!this.state.songs ? (
+          <CubeGrid color='white' size={80}/> //doesn't actually show but leave just in case..
+        ) : (
+          <Grid relaxed columns={4}>
+            {this.state.songs.map(song => {
+                return (
+                  <Grid.Column>
+                    <Popup trigger={<Image alt='' src={song.track.album.images[0].url}/>} 
+                      content={`Track name: ${song.track.name}         
+                                    Artist: ${song.track.artists[0].name}`}
+                      style={style}
+                    />
+                  </Grid.Column>
+                )
+              }
+              )}
+          </Grid>
+        )}
+      
+        <Button style={{borderRadius: '10'}} animated='fade' onClick={this.handleClick}>
           <Button.Content visible>See my weather tunecast!</Button.Content>
           <Button.Content hidden><Icon name='music'/></Button.Content>
         </Button>
@@ -94,12 +104,16 @@ class App extends React.Component {
 
 const style = {
   font: 'Varela Round',
+  color: 'white',
   borderRadius: 4,
-  backgroundColor: 'lavender',
+  backgroundColor: 'blue',
   fontWeight: 'bolder',
-  opacity: 0.75,
+  opacity: 0.7,
   padding: '2em',
 }
 
+//descriptions: light rain,heavy intensity rain, clear sky, few clouds, scattered clouds
+//can check main property which will say thunderstorm, drizzle, rain, etc. 
+//https://openweathermap.org/weather-conditions
 
 export default App;
